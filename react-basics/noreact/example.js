005fac27bbc0b5872b77ac75f9665ba4eebe1a83
Brian Shewan <brian.shewan@nscc.ca>
2023-01-10 08:29:27 -0400

    added initial website without react

---
+++ react-basics/noreact/example.js
@@ -0,0 +1,10 @@
+
+function div_onclick() {
+    const h1 = document.getElementById("heading");
+    h1.innerText = "Dynamic HTML";
+}
+
+const h1_onclick = () => {
+    const div = document.getElementById("main");
+    div.style.backgroundColor = "lightblue";
+};
