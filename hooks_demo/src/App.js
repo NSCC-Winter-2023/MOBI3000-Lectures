0f9080e4ed07ffd4cdf9f485be160dba0542ec2b
Brian Shewan <brian.shewan@nscc.ca>
2023-01-16 09:42:33 -0400

    finished hooks demo

--- hooks_demo/src/App.js
+++ hooks_demo/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Counter from "./Counter";
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(1);
+    const count_onclick = () => {
+        setCount(count * 2);
+    };
   return (
     <div className="App">
-      <Counter />
+      <Counter countBy={count} />
+      <button onClick={count_onclick}>Count By</button>
     </div>
   );
 }
--- hooks_demo/src/Counter.js
+++ hooks_demo/src/Counter.js
@@ -1,20 +1,51 @@
 import './Counter.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
-function Counter() {
+function Counter({countBy}) {
 
     const [count, setCount] = useState(0);
+    const [timer, setTimer] = useState(false);
 
-    let t;
     const start_onclick = () => {
-        t = setInterval(() => {
-            setCount(prev => prev + 1);
-        }, 1000);
+        setTimer(true);
     }
 
     const stop_onclick = () => {
-        clearInterval(t);
+        setTimer(false);
     }
+
+    // no dependency list means the useEffect will
+    // be called during every render
+    useEffect(() => {
+        console.log('useEffect called.');
+    })
+
+    // an empty dependency list will cause useEffect
+    // to only be called during Mounting
+    // the 'clearInterval' will be called when the
+    // component unmounts or when the dependency list
+    // changes
+    useEffect(() => {
+        let t = undefined;
+        if (timer) {
+            t = setInterval(() => {
+                setCount(prev => prev + countBy);
+            }, 1000);
+            return () => clearInterval(t);
+        } else {
+            clearInterval(t);
+        }
+    }, [countBy, timer]);
+
+    // a variable in the dependency list will cause
+    // useEffect to be called only when it changes
+    // and cause a re-render
+    useEffect(() => {
+        const h1 = document.getElementsByTagName('h1');
+        for (const h of h1) {
+            h.style.fontSize = `${count}px`;
+        }
+    }, [count])
 
     return (
         <div className="counter">
